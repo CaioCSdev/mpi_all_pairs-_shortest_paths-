5f0c1310bcfaf7b26fd218aa10b9c9eb37819fa8
Caio Salgado <caio.csalgado@gmail.com>
2024-11-11 20:49:55 +0000

    single thread works!

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <limits.h>
+#include <math.h>
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,6 +15,29 @@ Matrix read_file(char *filename);
 void print_location();
 void print_matrix(Matrix matrix);
 
+int min_in_ij(Matrix matrix, int i, int j) {
+  int min = matrix.vals[i][j];
+  int candidate;
+
+  for (int k = 0; k < matrix.n; k++) {
+    if (matrix.vals[i][k] == INT_MAX || matrix.vals[k][j] == INT_MAX)
+      continue;
+    candidate = matrix.vals[i][k] + matrix.vals[k][j];
+    if (candidate < min)
+      min = candidate;
+  }
+
+  return min;
+}
+
+void step(Matrix matrix) {
+  for (int i = 0; i < matrix.n; i++) {
+    for (int j = 0; j < matrix.n; j++) {
+      matrix.vals[i][j] = min_in_ij(matrix, i, j);
+    }
+  }
+}
+
 int main(int argc, char **argv) {
   int my_rank;
   MPI_Init(&argc, &argv);
@@ -22,6 +46,8 @@ int main(int argc, char **argv) {
 
   if (my_rank == 0) {
     Matrix matrix = read_file(argv[1]);
+    for (int i = 0; i < matrix.n - 2; i++)
+      step(matrix);
     print_matrix(matrix);
   }
 
@@ -48,6 +74,8 @@ Matrix read_file(char *filename) {
     matrix.vals[i] = (int *)malloc(matrix.n * sizeof(int));
     for (int j = 0; j < matrix.n; j++) {
       fscanf(file, "%d", &matrix.vals[i][j]);
+      if (i != j && matrix.vals[i][j] == 0)
+        matrix.vals[i][j] = INT_MAX;
     }
   }
 
@@ -57,9 +85,17 @@ Matrix read_file(char *filename) {
 }
 
 void print_matrix(Matrix matrix) {
+  int val;
+
   for (int i = 0; i < matrix.n; i++) {
-    for (int j = 0; j < matrix.n; j++)
-      printf("%d ", matrix.vals[i][j]);
+    for (int j = 0; j < matrix.n - 1; j++) {
+      val = matrix.vals[i][j] == INT_MAX ? 0 : matrix.vals[i][j];
+      printf("%d ", val);
+    }
+    val = matrix.vals[i][matrix.n - 1] == INT_MAX
+      ? 0
+      : matrix.vals[i][matrix.n - 1];
+    printf("%d", val);
     printf("\n");
   }
 }
