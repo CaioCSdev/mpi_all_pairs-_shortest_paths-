6fd2158d6418d7f9440cbe1dc91005fd5e232b0c
Caio Salgado <caio.csalgado@gmail.com>
2024-11-11 02:22:26 +0000

    adding files

--- Makefile
+++ Makefile
@@ -4,11 +4,11 @@ fox: main.c main.h
 	mpicc -Wall --pedantic -g -o fox main.c
 
 run: fox
-	@mpirun fox
+	@mpirun fox input
 
 deploy: fox
 	rsync --rsh 'ssh -J up202410254@ssh.alunos.dcc.fc.up.pt' --human-readable --recursive . up202410254@L102:/net/areas/homes/up202410254/classes/parallel/pa1
 
 run-cluster: deploy
-	@ssh -J up202410254@ssh.alunos.dcc.fc.up.pt up202410254@L102 -t 'cd classes/parallel/pa1; make fox ; mpirun --hostfile hostnames --use-hwthread-cpus fox'
+	@ssh -J up202410254@ssh.alunos.dcc.fc.up.pt up202410254@L102 -t 'cd classes/parallel/pa1; make fox ; mpirun --hostfile hostnames --use-hwthread-cpus fox input'
 
--- main.c
+++ main.c
@@ -1,22 +1,70 @@
 #include <limits.h>
-#include "main.h"
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc, char **argv)
-{
+typedef struct {
+  int n;
+  int **vals;
+} Matrix;
+
+Matrix read_file(char *filename);
+void print_location();
+void print_matrix(Matrix matrix);
+
+int main(int argc, char **argv) {
+  int my_rank;
   MPI_Init(&argc, &argv);
 
-  print_location();
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+  if (my_rank == 0) {
+    Matrix matrix = read_file(argv[1]);
+    print_matrix(matrix);
+  }
+
+  // print_location();
 
   MPI_Finalize();
   return 0;
 }
 
-void print_location()
-{
+Matrix read_file(char *filename) {
+  FILE *file = fopen(filename, "r");
+
+  if (file == NULL) {
+    printf("Error: Could not open file %s\n", filename);
+    Matrix empty_matrix = {0, NULL};
+    return empty_matrix;
+  }
+
+  Matrix matrix;
+  fscanf(file, "%d", &matrix.n);
+
+  matrix.vals = (int **)malloc(matrix.n * sizeof(int *));
+  for (int i = 0; i < matrix.n; i++) {
+    matrix.vals[i] = (int *)malloc(matrix.n * sizeof(int));
+    for (int j = 0; j < matrix.n; j++) {
+      fscanf(file, "%d", &matrix.vals[i][j]);
+    }
+  }
+
+  fclose(file);
+
+  return matrix;
+}
+
+void print_matrix(Matrix matrix) {
+  for (int i = 0; i < matrix.n; i++) {
+    for (int j = 0; j < matrix.n; j++)
+      printf("%d ", matrix.vals[i][j]);
+    printf("\n");
+  }
+}
+
+void print_location() {
   pid_t pid = getpid();
   char hostname[HOST_NAME_MAX + 1];
 
